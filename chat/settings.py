d2966eb49a52949557097e8fd74d1f1fc9df0ff5
LeonardoChirivi <leonardochirivi1@gmail.com>
2017-03-22 16:28:58 +0100

    typo

--- chat/settings.py
+++ chat/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
