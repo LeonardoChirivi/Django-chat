6582de621a4f0c5d7dcaf84fa05daa4d90f44ec6
LeonardoChirivi <leonardochirivi1@gmail.com>
2017-03-22 16:27:41 +0100

    static files

--- chat/settings.py
+++ chat/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
